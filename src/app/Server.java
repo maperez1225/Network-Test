425ac52194059750347f1a86f3c3309fe68dc31b
Martin Perez Lopez <martin.perez2@correo.icesi.edu.co>
2022-02-24 19:02:36 -0500

    Final commit

--- src/app/Server.java
+++ src/app/Server.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import com.Receiver.OnMessageListener;
 import com.ServerConnection;
-public class Server implements OnMessageListener{
+public class Server implements OnMessageListener {
 	private ServerConnection connection;
 	public Server() {
 		connection = ServerConnection.getInstance();
